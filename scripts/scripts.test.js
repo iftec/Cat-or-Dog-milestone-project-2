58724f4dbafea1dece4a8a1ac45fac1ac7e89282
iftec <iftec@users.noreply.github.com>
2023-11-18 11:41:56 +0000

    Create scripts.test.js
    Added 2 tests

---
+++ scripts/scripts.test.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+// scripts.test.js
+
+describe('makeGuess', () => {
+
+  test('increments score when guess matches actual', () => {
+    // Mock implementation 
+  });
+
+  test('ends game when guess does not match actual', () => {
+    // Mock implementation
+  });
+
+});
+
+
+
