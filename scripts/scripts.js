45561ee6cf0a9808209adc2229455a6c9acb4fed
iftec <iftec@users.noreply.github.com>
2023-11-18 10:23:39 +0000

    const created for images

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,3 +14,25 @@ const topScores = getTopScores();
 const restartGameButton = document.getElementById('restartGameButton');
 let score = 0;
 let gameOver = false;
+// Images used in the game
+const images = [
+  'images/cat1.jpg',
+  'images/cat2.jpg',
+  'images/cat3.jpg',
+  'images/cat4.jpg',
+  'images/cat5.jpg',
+  'images/cat6.jpg',
+  'images/cat7.jpg',
+  'images/cat8.jpg',
+  'images/cat9.jpg',
+  'images/cat10.jpg',
+  'images/dog1.jpg',
+  'images/dog2.jpg',
+  'images/dog3.jpg',
+  'images/dog5.jpg',
+  'images/dog6.jpg',
+  'images/dog7.jpg',
+  'images/dog8.jpg',
+  'images/dog9.jpg',
+  'images/dog10.jpg'
+];
